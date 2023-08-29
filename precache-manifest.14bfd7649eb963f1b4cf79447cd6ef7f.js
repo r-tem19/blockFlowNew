47d57959ecd8c9b693b854622cb4875e7e66ed80
r-tem19 <51026117+r-tem19@users.noreply.github.com>
2023-08-29 11:35:36 +0300

    Updates

---
+++ precache-manifest.14bfd7649eb963f1b4cf79447cd6ef7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86bf7b388e4ac6173117ba8da02a9557",
+    "url": "/blockFlowNew/index.html"
+  },
+  {
+    "revision": "2a70e15409e9c4c8b18c",
+    "url": "/blockFlowNew/static/css/main.d68588fa.chunk.css"
+  },
+  {
+    "revision": "10d6289f697679188cfa",
+    "url": "/blockFlowNew/static/js/2.a0e1677e.chunk.js"
+  },
+  {
+    "revision": "2a70e15409e9c4c8b18c",
+    "url": "/blockFlowNew/static/js/main.34006a5d.chunk.js"
+  },
+  {
+    "revision": "2e95f523d40bff570c1a",
+    "url": "/blockFlowNew/static/js/runtime-main.d761cd58.js"
+  }
+]);
